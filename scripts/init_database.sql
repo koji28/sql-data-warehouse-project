26a28339f3b9d00c0eeef7cabe09d0f70919dc49
KyiLwinThu <thukyilwoini@gmail.com>
2025-07-13 14:11:57 +0630

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,19 @@
+
+USE master;
+
+-- Create Database 'DataWarehouse'
+
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+-- Create Schemas
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
